686c1fa457bdee37c6dd05d9b8582f181837c3b9
Vikas Singh <vs@abc.com>
2024-04-20 01:18:16 +0530

    Implemented Caetgory List

---
+++ E-Commerce.Web/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using E_Commerce.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var category = _context.Categories.ToList();
+            return View(category);
+        }
+    }
+}
---
+++ E-Commerce.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using E_Commerce.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Vikas Singh", DisplayOrder=1},
+                new Category { Id=2, Name="History", DisplayOrder=2},
+                new Category { Id=3, Name="Sci-fi", DisplayOrder=3}
+                );
+        }
+    }
+}
